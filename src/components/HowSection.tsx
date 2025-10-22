ca8696183a4f7333d1c3fc866ca89b0cc5027dcd
Abisena Rais <abisena27@gmail.com>
2025-10-22 19:35:59 +0700

    Remove unused AOS import
    Removed unused AOS import from HowSection component.

--- src/components/HowSection.tsx
+++ src/components/HowSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 import CardWorks from './atom/CardWorks'
 import React, { useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default function HowSection() {
